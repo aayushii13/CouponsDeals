5141e958efc94b77a7f90adf28a279e30a6408e9
Aayushi <aayushiweb@gmail.com>
2024-03-29 08:57:12 +0530

    First Commit

---
+++ apiGateway/src/main/java/com/demo/trail/apiGateway/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.demo.trail.apiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+@EnableEurekaClient
+@CrossOrigin(origins = "http://localhost:4200")
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
